a616d23bcf7fcb769ff267fdc606e8f40bf6c1be
taybne <tasiafaustovatf@gmail.com>
2026-02-06 18:41:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,11 +82,11 @@ def get_locations(city_slug):
 
 @app.route("/")
 def index():
-    return render_template("index.html")  # ← Теперь найдёт в miniapp/templates!
-
+   return render_template("templates/index.html")
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
 
